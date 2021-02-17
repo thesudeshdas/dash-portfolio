0fffd4228f21febd00322dbfda88c4c4b1b23656
thesudeshdas <sudeshkumardas7@gmail.com>
2021-02-17 14:30:56 +0530

    added background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Background from "./components/Background";
+import NavigationBar from "./components/NavigationBar";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Background />
+      <Homepage />
+      {/* <NavigationBar /> */}
     </div>
   );
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavigationBar() {
+  return (
+    <>
+      <h1> Hello</h1>
+    </>
+  );
+}
+
+export default NavigationBar;
